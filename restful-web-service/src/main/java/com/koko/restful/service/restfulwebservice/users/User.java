fdc6c865b6c7a23660a37a2b0947ccee32c4b39a
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2023-01-20 04:02:45 -0600

    Learnt use of filters for REST endpoints

--- restful-web-service/src/main/java/com/koko/restful/service/restfulwebservice/users/User.java
+++ restful-web-service/src/main/java/com/koko/restful/service/restfulwebservice/users/User.java
@@ -2,6 +2,8 @@ package com.koko.restful.service.restfulwebservice.users;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,9 +12,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min=2, message="Name must be at least 2 charcters")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message="Birthdate should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	
